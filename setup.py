af0c176f12c9ed748f3303d013e473fddcb07ee8
Matthias W. Smith <mwsmith2@uw.edu>
2015-07-09 11:19:19 -0500

    Initial commit for mhelper utility.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mhelper',
+      version='0.1',
+      description='MIDAS Helper Utilities',
+      author='Matthias W. Smith',
+      author_email='mwsmith2@uw.edu',
+      py_modules=['mhelper'],
+     )
